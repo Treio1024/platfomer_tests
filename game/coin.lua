33ff22c420408022c02308ce0a1b47503e5a49b9
Treio Craft <paulo.lucas.s3311@gmail.com>
2025-01-25 17:20:39 -0300

    upgraded the coin.lua code

--- game/coin.lua
+++ game/coin.lua
@@ -3,11 +3,16 @@ Coin.sound = love.audio.newSource('assets/audio/coinSound.mp3', 'static')
 aCoins = {}
 
 function Coin.new(x, y)
-    local i = setmetatable(
-        {x = x, y = y, image = love.graphics.newImage'assets/gold_coin.png'}, Coin)
+    local i = setmetatable({}, Coin)
+
+    image = love.graphics.newImage'assets/gold_coin.png'
+
+    i.x = x; i.y = y
     i.width, i.height = i.image:getDimensions()
+    
     i.scaleX = 0
     i.spinRandom = love.math.random(1.2754910, 2.120194)
+    
     i.remove = false
 
     ----------------------------------------------------------------------------------------------------
@@ -20,57 +25,28 @@ function Coin.new(x, y)
     return i
 end
 
-function Coin:update(dt)
-    self:spin(dt)
-    self:check()
-end
-
-function Coin:draw()
-    love.graphics.draw(self.image, self.x / 2 + self.width, self.y / 2 + self.height, 0, self.scaleX, 1, self.width / 2, self.height / 2)
-end
-
-----------------------------------------------------------------------------------------------------
-
-function Coin.updateA(dt)
-    for _, v in ipairs(aCoins) do
-        v:update(dt)
-    end
-end
-
-function Coin.drawA()
-    for _, v in ipairs(aCoins) do
-        v:draw()
-    end
-end
-
-function Coin:spin(dt)
-    self.scaleX = math.sin(love.timer.getTime() * self.spinRandom)
-end
-
-function Coin:removeCoin()
+function Coin.update(dt)
     for i, v in ipairs(aCoins) do
-        if v == self then
+        v.scaleX = math.sin(love.timer.getTime() * v.spinRandom)
+
+        if v.remove then
             player.coinsAmount = player.coinsAmount + 1
-            self.collider.body:destroy()
+            v.collider.body:destroy()
             table.remove(aCoins, i)
+
+            Coin.sound:stop()
+            Coin.sound:play()
         end
     end
 end
 
-function Coin:removeALL()
-    for i, v in ipairs(aCoins) do
-        v.collider:destroy()
-        aCoins[i] = nil
+function Coin.draw()
+    for _, v in ipairs(aCoins) do
+        love.graphics.draw(v.image, v.x / 2 + v.width, v.y / 2 + v.height, 0, v.scaleX, 1, v.width / 2, v.height / 2)
     end
 end
 
-function Coin:check()
-    if self.remove then
-        self:removeCoin()
-        Coin.sound:stop()
-        Coin.sound:play()
-    end
-end
+----------------------------------------------------------------------------------------------------
 
 function Coin.beginContact(a, b, contact)
     for _, v in ipairs(aCoins) do
