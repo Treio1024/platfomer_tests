29c68b04600daa05442bae0da04c3cb8069b2a9a
Treio Craft <paulo.lucas.s3311@gmail.com>
2025-01-25 17:22:48 -0300

    implemented hearts to display player health

--- game/gui.lua
+++ game/gui.lua
@@ -5,14 +5,24 @@ function gui:load()
 
     self.detailsOn = false
 
-    self:loadCoin()
+    self.coin.image = love.graphics.newImage'assets/gold_coin.png'
+    self.coin.width, self.coin.height = self.coin.image:getDimensions()
+    self.coin.x, self.coin.y = 10, 75
+
+    self.hearts = {}
+    self.hearts.array = love.graphics.newArrayImage({'assets/heart.png', 'assets/heart.png', 'assets/heart.png'})
+    self.hearts.array2 = love.graphics.newArrayImage({'assets/deadheart.png', 'assets/deadheart.png', 'assets/deadheart.png'})
+    self.hearts.x, self.hearts.y = 10, 10
+    self.hearts.x2 = 0
 end
 
 function gui:update(dt)
-    self:changeCoinPos()
+    self:changeGuiPos()
 end
 
 function gui:draw()
+    self:drawHearts()
+
     self:drawCoin()
     self:showDetails()
 end
@@ -27,6 +37,15 @@ function gui:showDetails()
     end
 end
 
+function gui:showDetailsOnCamera()
+    if gui.detailsOn then
+        world:draw(0xff)
+            
+        love.graphics.circle('fill', player.x, player.y, 5)
+        love.graphics.circle('fill', player.x - player.width / 2, player.y - player.height / 2, 5)
+    end
+end
+
 function gui:enableDetails(key)
     if key == "f3" then
         self.detailsOn = not self.detailsOn
@@ -35,21 +54,31 @@ end
 
 ----------------------------------------------------------------------------------------------------
 
-function gui:loadCoin()
-    self.coin.image = love.graphics.newImage'assets/gold_coin.png'
-    self.coin.width, self.coin.height = self.coin.image:getDimensions()
-    self.coin.x, self.coin.y = 10, 10
-end
-
 function gui:drawCoin()
     love.graphics.draw(self.coin.image, self.coin.x, self.coin.y, 0, 2)
     love.graphics.print(tostring(player.coinsAmount), self.coin.x + self.coin.width * 2.5, self.coin.y + 2, 0, 2)
 end
 
-function gui:changeCoinPos()
+function gui:drawHearts()
+    for i=1, 3 do
+        love.graphics.drawLayer(self.hearts.array2, i, self.hearts.x, self.hearts.y, 0, 3)
+        self.hearts.x = self.hearts.x + 64
+    end
+    self.hearts.x = self.hearts.x2
+
+    for i=1, player.health do
+        love.graphics.drawLayer(self.hearts.array, i, self.hearts.x, self.hearts.y, 0, 3)
+        self.hearts.x = self.hearts.x + 64
+    end
+    self.hearts.x = self.hearts.x2
+end
+
+function gui:changeGuiPos()
     if self.detailsOn then
-        self.coin.x, self.coin.y = 150, 10
+        self.coin.x = 150
+        self.hearts.x2 = 150
     else
-        self.coin.x, self.coin.y = 10, 10
+        self.coin.x = 10
+        self.hearts.x2 = 10
     end
 end
