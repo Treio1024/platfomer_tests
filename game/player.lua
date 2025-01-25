de5bc7a4ac27e661178a0215798843ce054f3bff
Treio Craft <paulo.lucas.s3311@gmail.com>
2025-01-25 09:03:34 -0300

    implemented spikes and health system to the player

--- game/player.lua
+++ game/player.lua
@@ -1,12 +1,10 @@
 player = {}
 
-function player:load() --principal functions
-    self.x = 64; self.y = 1312
-    self.startx = self.x; self.starty = self.y
-    self.endx = 1235
+function player:load()
+    self.x = map.current_level.properties.startX; self.y = map.current_level.properties.startY
     self.width = 48 * 2; self.height = 80 * 2
     self.xvel = 0; self.yvel = 0
-    self.MAX_SPEED = 750
+    self.maxSpeed = 750
     self.ACCELERATION = 3500
     self.FRICTION = 6000
     self.GRAVITY = 1250
@@ -15,13 +13,20 @@ function player:load() --principal functions
     self.graceTime = 0
     self.direction = ""; self.onTheWall = false
     self.coinsAmount = 0
+    self.health = 3
+    self.damaged = false; self.death_delay = 0.5
+    self.damageSound = love.audio.newSource('assets/audio/pipefalling.mp3', 'static')
+
+    ----------------------------------------------------------------------------------------------------
 
     self.collider = world:newRectangleCollider(self.x, self.y, self.width, self.height)
     self.collider:setType('dynamic')
     self.collider:setFixedRotation(true)
 
-    self.spritesheet = love.graphics.newImage'images/spritesheet.png'
-    self.grid = anim8.newGrid(48, 80, 288, 160)
+    ----------------------------------------------------------------------------------------------------
+
+    self.spritesheet = love.graphics.newImage'assets/spritesheet.png'
+    self.grid = anim8.newGrid(48, 80, 336, 160)
     
     self.animations = {}
     self.animations.walk_right = anim8.newAnimation(self.grid('1-4', 1), 0.2)
@@ -33,22 +38,27 @@ function player:load() --principal functions
     self.animations.idle_right = anim8.newAnimation(self.grid('6-6', 1), 1)
     self.animations.idle_left = anim8.newAnimation(self.grid('6-6', 2), 1)
 
+    self.animations.damaged_right = anim8.newAnimation(self.grid('7-7', 1), 1)
+    self.animations.damaged_left = anim8.newAnimation(self.grid('7-7', 2), 1)
+    
     self.animations.actual = self.animations.idle_right
 end
 
 function player:update(dt)
-    self:backToStart()
     self:syncPhysics()
     --self:decreaseGraceTime(dt)
     self:move(dt)
     self:setState()
     self.animations.actual:update(dt)
     self:applyGravity(dt)
+    self:afterDamage(dt)
 end
 
 function player:draw() 
     self.animations.actual:draw(self.spritesheet, (self.x / 2) - 24, (self.y / 2) - 40)
-end --end
+end
+
+----------------------------------------------------------------------------------------------------
 
 function player:setState()
     if not self.onGround then
@@ -69,12 +79,12 @@ end
 function player:move(dt)
     if love.keyboard.isDown'right' then
         
-        self.xvel = math.min(self.xvel + self.ACCELERATION * dt, self.MAX_SPEED)
+        self.xvel = math.min(self.xvel + self.ACCELERATION * dt, self.maxSpeed)
         self.direction = "right"
         self.animations.actual = self.animations.walk_right
     elseif love.keyboard.isDown'left' then
         
-        self.xvel = math.max(self.xvel - self.ACCELERATION * dt, -self.MAX_SPEED)
+        self.xvel = math.max(self.xvel - self.ACCELERATION * dt, -self.maxSpeed)
         self.direction = "left"
         self.animations.actual = self.animations.walk_left
     else
@@ -95,19 +105,23 @@ end
 function player:applyGravity(dt)
     if not self.onGround and self.yvel < 800 then
         self.yvel = self.yvel + self.GRAVITY * dt
+        if self.maxSpeed > 400 then
+            self.maxSpeed = self.maxSpeed - dt * 1000
+        end
+    else
+        if self.maxSpeed < 750 then
+            self.maxSpeed = self.maxSpeed + dt * 10000
+        end
     end
 end
 
 function player:jump(key)
     if key == 'space' or key == 'up' then
-        --print(self.onGround)
         if self.onGround then
             self.yvel = -750
-            --print'jumped'
-        elseif self.jumpCounter > 0 and self.graceTime > 0 then -- double jump
+        elseif self.jumpCounter > 0 and self.graceTime > 0 then 
             self.yvel = -750 * 0.8
             self.jumpCounter = self.jumpCounter - 1
-            --print'double_jumped'
         end
     end
 end
@@ -122,24 +136,13 @@ function player:land(collision)
     self.currentGroundCollision = collision
     self.yvel = 0
     self.onGround = true
-    self.jumpCounter = 90
+    self.jumpCounter = 60
     self.graceTime = 0.9
 end
 
-function player:backToStart()
-    if self.x > map.levels[map.current_level].properties.endX and (self.y >= map.levels[map.current_level].properties.endY2 or self.y <= map.levels[map.current_level].properties.endY1) then
-    
-        map:next(true)
-        self.collider:setPosition(map.levels[map.current_level].properties.startX, map.levels[map.current_level].properties.startY)
-    elseif self.x < 0 and (self.y >= map.levels[map.current_level].properties.startY or self.y <= map.levels[map.current_level].properties.startY) then
-        
-        map:next(false)
-        self.collider:setPosition(map.levels[map.current_level].properties.endX - 32, map.levels[map.current_level].properties.endY1)
-    end
-end --end
+----------------------------------------------------------------------------------------------------
 
-function player:beginContact(a, b, collision) --collision callbacks
-    --print'contact_started'
+function player:beginContact(a, b, collision)
     local nx, ny = collision:getNormal()
     
     if not self.onGround then
@@ -169,7 +172,6 @@ function player:beginContact(a, b, collision) --collision callbacks
 end
 
 function player:endContact(a, b, collision)
-    --print'contact_ended'
     local nx, ny = collision:getNormal()
 
     if a == player.collider.fixture or b == player.collider.fixture then
@@ -181,7 +183,39 @@ function player:endContact(a, b, collision)
             self.onTheWall = false
         end
     end 
-end --end
+end
+
+----------------------------------------------------------------------------------------------------
+
+function player:takeDamage(val)
+    self.health = self.health - val
+    self.damaged = true
+    self.damageSound:play()
+end
+
+function player:afterDamage(dt)
+    if self.damaged then
+        self.xvel, self.yvel = 0, 0
+        
+        if self.direction == 'right' then
+            self.animations.actual = self.animations.damaged_right
+        else
+            self.animations.actual = self.animations.damaged_left    
+        end
+
+
+        self.death_delay = self.death_delay - dt
+        
+        if self.death_delay <= 0 then
+            player.collider:setPosition(map.current_level.properties.startX, map.current_level.properties.startY)
+            self.damaged = false
+            self.death_delay = 0.5
+            return
+        end
+    end
+end
+
+----------------------------------------------------------------------------------------------------
 
 function player:syncPhysics()
     self.x, self.y = self.collider:getPosition()
---
+++ game/spikes.lua
@@ -0,0 +1,41 @@
+spikes = {}; spikes.__index = spikes
+aSpikes = {}
+
+function spikes.new(x, y, width, height)
+    local i = setmetatable({}, spikes)
+
+    i.image = love.graphics.newImage'assets/spikes.png'
+    i.width, i.height = width, height
+    i.x, i.y = x, y
+
+    i.collider = world:newRectangleCollider(i.x * 2, i.y * 2, width, height)
+    i.collider:setType('static')
+    i.collider:setSensor(true)
+    table.insert(aSpikes, i)
+
+    return i
+end
+
+function spikes.draw()
+    for i, obj in pairs(aSpikes) do
+        for i=0, obj.width / 64 - 1 do
+            love.graphics.draw(obj.image, obj.x + (i * 32), obj.y)
+        end
+    end
+end
+
+function spikes.remove()
+    for i, v in pairs(aSpikes) do
+        v.collider:destroy()
+        aSpikes[i] = nil
+    end
+end
+
+function spikes.beginContact(a, b, contact)
+    for _, v in ipairs(aSpikes) do
+        if (a == v.collider.fixture or b == v.collider.fixture) and (a == player.collider.fixture or b == player.collider.fixture) then
+            player:takeDamage(1)
+            return true
+        end
+    end
+end
